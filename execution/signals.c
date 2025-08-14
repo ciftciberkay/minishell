fe495dff8f277e4d1cb7f053c3a8fa0c751b4f45
ciftciberkay <berko.berko2016@gmail.com>
2025-08-14 20:32:07 +0300

    of

--- execution/signals.c
+++ execution/signals.c
@@ -6,7 +6,7 @@
 /*   By: macbook6 <macbook6@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 13:39:53 by femullao          #+#    #+#             */
-/*   Updated: 2025/08/14 20:29:53 by macbook6         ###   ########.fr       */
+/*   Updated: 2025/08/14 20:31:31 by macbook6         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	sigint_handler(int ptr)
 {
 	(void)ptr;
 	ft_putchar_fd('\n', STDOUT_FILENO);
-	//rl_replace_line("", 0);
+	rl_replace_line("", 0);
 	rl_on_new_line();
 	rl_redisplay();
 }
